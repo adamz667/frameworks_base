98cd40c525d686a2ff7ec3ef86479390ebed713b
Jacek Adamski <adams@g.pl>
2013-02-02 20:02:32 +0100

    Revert "Add support for HW Rotation Lock"
    This reverts commit b8d93ea9c550de160267218f7c09552eb7c92909.
    
    Conflicts:
    
    	core/res/res/values/config.xml
    	core/res/res/values/symbols.xml
    
    Change-Id: Ic5af3ffa69008afa687b456f201ce2cbdb7685a0

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -144,7 +144,6 @@ class ServerThread extends Thread {
         WindowManagerService wm = null;
         BluetoothManagerService bluetooth = null;
         DockObserver dock = null;
-        RotationSwitchObserver rotateSwitch = null;
         UsbService usb = null;
         SerialService serial = null;
         TwilightService twilight = null;
@@ -344,9 +343,6 @@ class ServerThread extends Thread {
             Slog.e("System", "******************************************");
             Slog.e("System", "************ Failure starting core service", e);
         }
-
-        boolean hasRotationLock = context.getResources().getBoolean(com.android
-                .internal.R.bool.config_hasRotationLockSwitch);
 
         DevicePolicyManagerService devicePolicy = null;
         StatusBarManagerService statusBar = null;
@@ -638,17 +634,7 @@ class ServerThread extends Thread {
             }
 
             try {
-                if (hasRotationLock) {
-                    Slog.i(TAG, "Rotation Switch Observer");
-                    // Listen for switch changes
-                    rotateSwitch = new RotationSwitchObserver(context);
-                }
-            } catch (Throwable e) {
-                reportWtf("starting RotationSwitchObserver", e);
-            }
-
-            try {
-                Slog.i(TAG, "Wired Accessory Observer");
+                Slog.i(TAG, "Wired Accessory Manager");
                 // Listen for wired headset changes
                 inputManager.setWiredAccessoryCallbacks(
                         new WiredAccessoryManager(context, inputManager));
@@ -872,7 +858,6 @@ class ServerThread extends Thread {
         final NetworkPolicyManagerService networkPolicyF = networkPolicy;
         final ConnectivityService connectivityF = connectivity;
         final DockObserver dockF = dock;
-        final RotationSwitchObserver rotateSwitchF = rotateSwitch;
         final UsbService usbF = usb;
         final ThrottleService throttleF = throttle;
         final TwilightService twilightF = twilight;
@@ -935,11 +920,6 @@ class ServerThread extends Thread {
                     if (dockF != null) dockF.systemReady();
                 } catch (Throwable e) {
                     reportWtf("making Dock Service ready", e);
-                }
-                try {
-                    if (rotateSwitchF != null) rotateSwitchF.systemReady();
-                } catch (Throwable e) {
-                    reportWtf("making Rotation Switch Service ready", e);
                 }
                 try {
                     if (usbF != null) usbF.systemReady();
